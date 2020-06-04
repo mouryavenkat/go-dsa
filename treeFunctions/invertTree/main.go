1c2d54651ce446988adc7a3ecb00cf31aaae4f9c
mourya <gunda.mourya@go-mmt.com>
2020-06-04 20:37:23 +0530

    Invert Binary Tree

---
+++ treeFunctions/invertTree/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "go-dsa/treeformation"
+
+/*
+	Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+ */
+func invertTree(root *treeformation.Node) *treeformation.Node {
+	if root == nil {
+		return root
+	}
+	temp := root.Left
+	root.Left = root.Right
+	root.Right = temp
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
