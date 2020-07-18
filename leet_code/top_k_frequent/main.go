dab7e40a2cb1f77f31f944e136856424564c6313
mourya <gunda.mourya@go-mmt.com>
2020-07-18 18:50:25 +0530

    Top K Frequent Nodes

---
+++ leet_code/top_k_frequent/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "go-dsa/heap"
+
+func topKFrequent(nums []int, k int) []int {
+	h := heap.NewHeap()
+	numsMap := map[int]int{}
+	for _, num := range nums {
+		numsMap[num]++
+	}
+	for key, value := range numsMap {
+		h.Insert(&heap.Node{
+			Val:  key,
+			Data: value,
+		})
+	}
+	returnData := make([]int, k)
+	for i := 0; i < k; i++ {
+		returnData[i] = h.Delete().Val
+
+	}
+	return returnData
+}
+
+func main() {
+	/*
+		Given a non-empty array of integers, return the k most frequent elements.
+
+		Example 1:
+
+		Input: nums = [1,1,1,2,2,3], k = 2
+		Output: [1,2]
+		Example 2:
+
+		Input: nums = [1], k = 1
+		Output: [1]
+		Note:
+
+		You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+		Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+		It's guaranteed that the answer is unique, in other words the set of the top k frequent elements is unique.
+		You can return the answer in any order.
+	*/
+}
