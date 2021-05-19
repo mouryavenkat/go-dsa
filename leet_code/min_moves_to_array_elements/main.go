2512b9d05d5e67f2f22f12230c3c6d1103e1e7d7
mouryaibibo <gunda.mourya@go-mmt.com>
2021-05-19 17:42:00 +0530

    Minimum moves to make elements equal in array

---
+++ leet_code/min_moves_to_array_elements/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "sort"
+
+/*
+	Given an integer array nums of size n, return the minimum number of moves required to make all array elements equal.
+
+	In one move, you can increment or decrement an element of the array by 1
+
+	Input: nums = [1,2,3]
+	Output: 2
+	Explanation:
+	Only two moves are needed (remember each move increments or decrements one element):
+	[1,2,3]  =>  [2,2,3]  =>  [2,2,2]
+
+	Input: nums = [1,10,2,9]
+	Output: 16
+ */
+func minMoves2(nums []int) int {
+	sort.Ints(nums)
+	var moves int
+	if len(nums)%2 != 0 {
+		midIndex := (len(nums) - 1) / 2
+		for i := 0; i < midIndex; i++ {
+			moves += nums[midIndex] - nums[i]
+		}
+		for i := midIndex + 1; i < len(nums); i++ {
+			moves += nums[i] - nums[midIndex]
+		}
+	} else {
+		midElement := (nums[len(nums)/2] + nums[(len(nums)-2)/2]) / 2
+		var i int
+		for nums[i] <= midElement {
+			moves += midElement - nums[i]
+			i++
+		}
+		for i < len(nums) {
+			moves += nums[i] - midElement
+			i++
+		}
+	}
+	return moves
+}
